a4cca2e3bdd1ca4146df4ea2d2a7a552a3163222
Pablo Viojo <pviojo@gmail.com>
2020-06-02 11:17:47 -0400

    Update 20200602

--- src/data.js
+++ src/data.js
@@ -1,7 +1,6 @@
-export const lastUpdate = "2020-05-31 21:00:00";
-export const lastCount = 105159;
-export const prevCount = 90638;
+export const lastUpdate = "2020-06-01 21:00:00";
+export const lastCount = 108686;
+export const prevCount = 99688;
 
-
-export const lastDeath = 1113;
-export const prevDeath = 944;
+export const lastDeath = 1188;
+export const prevDeath = 997;
